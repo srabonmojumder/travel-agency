bfc95312f2f1d31a0286fe0ebd07d38392044006
srabonmozumder2 <118354471+srabonmozumder2@users.noreply.github.com>
2023-12-04 20:28:19 +0600

    all init

---
+++ src/assets/js/main.js
@@ -0,0 +1,254 @@
+//---------SEARCH BAR START----------//
+$(document).ready(function () {
+    const searchIcon = document.querySelector(`.search-icon`)
+    const searchInput = document.querySelector(`.search-input`)
+
+    const toggleSearch = event => {
+        event.stopPropagation();
+
+        if (!event.target.closest('.search-input')) {
+
+            searchInput.classList.toggle('active');
+
+            searchInput.classList.contains('active')
+                ? document.addEventListener('click', toggleSearch)
+                : document.removeEventListener('click', toggleSearch);
+        }
+    }
+
+    searchIcon.addEventListener('click', toggleSearch);
+
+    //---------SEARCH BAR END----------//
+
+
+    //------------Best Tour Packages slider Start---------- 
+
+    $('.main-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        margin: 10,
+        autoplay: false,
+        margin: '10px',
+        dots: true,
+        arrows: true,
+        prevArrow: "<div class='nav-bar-left-arrow'><i class='fa-solid fa-chevron-left'></i></div>",
+        nextArrow: "<div class='nav-bar-right-arrow'><i class='fa-solid fa-chevron-right'></i></div>",
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 1008,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
+
+
+    //------------Best Tour Packages slider End----------->
+
+
+    //-----------Special Packages slider Start------------>
+
+    $('.main-special-packages-slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: false,
+        arrows: true,
+        dots: true,
+        prevArrow: "<div class='left-nav-bar'><i class='fa-solid fa-chevron-left'></i></div>",
+        nextArrow: "<div class='right-nav-bar'><i class='fa-solid fa-chevron-right'></i></div>",
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 1008,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
+    // <!-- ---------------Special Packages slider End---------->
+
+
+    //   <!-- ---------------Instagram slider Start------------>
+
+    $('.instagram-slider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: false,
+        infinite: true,
+        arrows: true,
+        prevArrow: "<div class='nav-bar-left-arrow'><i class='fa-solid fa-chevron-left'></i></div>",
+        nextArrow: "<div class='nav-bar-right-arrow'><i class='fa-solid fa-chevron-right'></i></div>",
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 1008,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
+
+    // -----------imgae overlay start---------->
+    $(".hover").mouseleave(
+        function () {
+            $(this).removeClass("hover");
+        }
+    );
+
+    // -----------imgae overlay End------------>
+
+    // <-------Instagram slider End----------->
+
+
+    //   <-----------TESTIMONIAL slider Start-------->
+
+    $('.testimonial-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: false,
+        arrows: false,
+        dots: true,
+        // prevArrow: "<div class='instagram-left-nav'><i class='fa fa-arrow-left'></i></div>",
+        // nextArrow: "<div class='instagram-right-nav'><i class='fa fa-arrow-right'></i></div>",
+        responsive: [
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+        ]
+    });
+    // <----------TESTIMONIAL slider End------------>
+
+    //   <------------Our  Blog slider Start------------>
+
+    $('.our-latest-news-blog').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        arrows: true,
+        prevArrow: "<div class='nav-bar-left-arrow'><i class='fa-solid fa-chevron-left'></i></div>",
+        nextArrow: "<div class='nav-bar-right-arrow'><i class='fa-solid fa-chevron-right'></i></div>",
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 1008,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
+    // <----------Our  Blog slider  End---------->
+
+
+
+
+
+
+
+
+
+
+
+        //   <------------Our  Blog slider Start------------>
+
+        $('.our-latest-news-blog-two').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            autoplay: false,
+            arrows: true,
+            prevArrow: "<div class='nav-bar-left-arrow'><i class='fa-solid fa-chevron-left'></i></div>",
+            nextArrow: "<div class='nav-bar-right-arrow'><i class='fa-solid fa-chevron-right'></i></div>",
+            responsive: [
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                    },
+                },
+                {
+                    breakpoint: 1008,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    },
+                },
+                {
+                    breakpoint: 800,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    },
+                },
+            ],
+        });
+        // <----------Our  Blog slider  End---------->
+
+});
+
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    fontFamily: {
+      'inter': ['Inter', 'sans-serif'],
+      'Messiri': ['El Messiri', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'daily-dev-tips': "#F89283"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
