40f64f7cb5741f1a864934f70eb6e7c3c7d740fc
arafat-rifat <arafat15-12603@diu.edu.bd>
2024-01-10 10:40:02 +0600

    Fixing Responsive Issue and Slider Issue

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,21 +1,39 @@
 "use strict";
 
-const sliderContent = document.querySelector(".slider-content");
-const slides = document.querySelectorAll(".slider-content > div");
-const slideWidth = slides[0].offsetWidth;
-let currentIndex = 0;
+// import Swiper from "swiper/bundle"; // Make sure to use 'swiper/bundle' to import the bundled version
+// import "swiper/swiper-bundle.min.css"; // Import Swiper styles
 
-function nextSlide() {
-  currentIndex = (currentIndex + 1) % slides.length;
-  updateSliderPosition();
-}
+// const sliderContent = document.querySelector(".slider-content");
+// const slides = document.querySelectorAll(".slider-content > div");
+// const slideWidth = slides[0].offsetWidth;
+// let currentIndex = 0;
 
-function prevSlide() {
-  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-  updateSliderPosition();
-}
+// function nextSlide() {
+//   currentIndex = (currentIndex + 1) % slides.length;
+//   updateSliderPosition();
+// }
 
-function updateSliderPosition() {
-  const newPosition = -currentIndex * slideWidth;
-  sliderContent.style.transform = `translateX(${newPosition}px)`;
-}
+// function prevSlide() {
+//   currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+//   updateSliderPosition();
+// }
+
+// function updateSliderPosition() {
+//   const newPosition = -currentIndex * slideWidth;
+//   sliderContent.style.transform = `translateX(${newPosition}px)`;
+// }
+
+// This is for Swipper Js
+var swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".nextButton",
+    prevEl: ".prevButton",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+  slidesPerview: 2,
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,40 +2,39 @@ module.exports = {
   content: ["./src/*.html"],
   theme: {
     fontFamily: {
-      'inter': ['Inter', 'sans-serif'],
-      'Messiri': ['El Messiri', 'sans-serif'],
+      inter: ["Inter", "sans-serif"],
+      Messiri: ["El Messiri", "sans-serif"],
     },
     screens: {
-      '2xl': { 'max': '1535px' },
+      "2xl": { max: "1535px" },
       // => @media (max-width: 1535px) { ... }
 
-      'xl': { 'max': '1279px' },
+      xl: { max: "1279px" },
       // => @media (max-width: 1279px) { ... }
 
-      'lg': { 'max': '991px' },
+      lg: { max: "991px" },
       // => @media (max-width: 991px) { ... }
 
-      'md': { 'max': '767px' },
+      md: { max: "767px" },
       // => @media (max-width: 767px) { ... }
 
-      'sm': { 'max': '575px' },
+      sm: { max: "575px" },
       // => @media (max-width: 575px) { ... }
 
-      'exsm': { 'max': '480px' },
+      exsm: { max: "480px" },
       // => @media (max-width: 575px) { ... }
-    }
-    ,
+    },
     extend: {
       colors: {
-        'daily-dev-tips': "#F89283"
-      }
+        "daily-dev-tips": "#F89283",
+      },
     },
   },
   variants: {
     extend: {
-      display: ['group-hover'],
-      visibility: ['group-hover'],
+      display: ["group-hover"],
+      visibility: ["group-hover"],
     },
   },
   plugins: [],
-}
+};
